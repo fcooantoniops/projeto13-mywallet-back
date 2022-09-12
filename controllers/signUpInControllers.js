2930e8d0d4b3ae264d1cd83e9aa89909b02d2d6b
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2022-09-12 14:42:18 -0300

    SignIn Post ready

--- controllers/signUpInControllers.js
+++ controllers/signUpInControllers.js
@@ -1,10 +1,15 @@
 import joi from "joi";
 import bcrypt from 'bcrypt';
-//import {v4 as uuid} from 'uuid';
+import {v4 as uuid} from 'uuid';
 import db from '../database/db.js';
 
 const signUpSchema = joi.object({
     name: joi.string().required().min(1),
+    email: joi.string().required().email().min(1),
+    password: joi.string().required().min(1)
+});
+
+const signInSchema = joi.object({
     email: joi.string().required().email().min(1),
     password: joi.string().required().min(1)
 });
@@ -33,4 +38,31 @@ async function postSignUp (res, req) {
     };
 };
 
-export { postSignUp };
+async function postSignIn (req,res) {
+    const {email, password} = req.body;
+    const validation = signInSchema.validate({email, password});
+    if (validation.error){
+        res.status(422).send("error");
+        return;
+    };
+
+    try{
+        const user = await db.collection('users').findOne({email});
+        const validatePassword = bcrypt.compareSync(password, user.password);
+
+        if (user && validatePassword){
+            const token = uuid();
+            await db.collection("sessions").insertOne({
+                userId: user._id,
+                token
+            });
+            return res.send({token, name: user.name});
+        } else {
+            return res.sendStatus(422).send("password invalid");
+        };
+    } catch {
+        res.sendStatus(422);
+    };
+};
+
+export { postSignUp, postSignIn };
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { express } from "express";
 import { cors } from "cors";
 
-import { postSignUp } from './Controllers/authController.js';
+import { postSignUp, postSignIn } from './Controllers/authController.js';
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
 server.post('/sign-up', postSignUp);
+
+server.post('/', postSignIn);
 
 server.listen(5000);
